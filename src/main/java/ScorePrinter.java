1a5f23a4c7f484f371bbd83038efe54ef15d8df8
Bacon <parkerjoshua0403@gmail.com>
2026-02-06 19:17:53 -0500

    Todos Completed

--- src/main/java/ScorePrinter.java
+++ src/main/java/ScorePrinter.java
@@ -13,41 +13,30 @@ public class ScorePrinter {
             int homeworkScore,
             int examScore,
             int homeworkWeight,
-            int examWeight)
+            int examWeight) {
 
-        int totalWeight = homeworkWeight + examWeight
+        int totalWeight = homeworkWeight + examWeight;
 
         // TODO 1:
-        // Print the homework score and exam score on one line.
-        // Format:
-        // Homework Score and Exam Score: <homeworkScore> <examScore>
         System.out.print("Homework Score and Exam Score: ");
-        System.out.print(homeworkScore + "");
+        System.out.print(homeworkScore + " ");
         System.out.println(examScore);
 
         // TODO 2:
-        // Print the homework weight and exam weight on one line.
-        // Format:
-        // Homework Weight and Exam Weight: <homeworkWeight> <examWeight>
-
         System.out.print("Homework Weight and Exam Weight: ");
-        System.out.print(homeworkWeight + "");
+        System.out.print(homeworkWeight + " ");
         System.out.println(examWeight);
 
         double homeworkPart =
-                homeworkScore * homeworkWeight / totalWeight;
+                (double) homeworkScore * homeworkWeight / totalWeight;
 
         double examPart =
-                examScore * examWeight / totalWeight;
+                (double) examScore * examWeight / totalWeight;
 
         System.out.println("Homework contribution: " + homeworkPart);
         System.out.println("Exam contribution: " + examPart);
 
         // TODO 3:
-        // Print the final score.
-        // Format:
-        // Final score: <sum of homeworkPart and examPart>
-        System.out.println("Final score: " + homeworkPart + examPart);
+        System.out.println("Final score: " + (homeworkPart + examPart));
     }
 }
-
